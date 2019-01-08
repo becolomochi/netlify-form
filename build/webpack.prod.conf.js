5fbb5911f99a7fbac7d0381944d9b0df1e8f6c49
becolomochi <becolomochi@gmail.com>
2019-01-08 22:12:39 +0900

    Add package Pre Render

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
 const env = require('../config/prod.env')
 
@@ -120,7 +121,16 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsRoot,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new PrerenderSPAPlugin(
+      // Absolute path to compiled SPA
+      path.resolve(__dirname, 'dist'),
+      // List of routes to prerender
+      [ '/'],
+      {
+        // options
+      }
+    )
   ]
 })
 
