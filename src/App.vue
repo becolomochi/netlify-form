f764582388fdf43248ba705a9c15acfbc23744d5
becolomochi <becolomochi@gmail.com>
2019-01-08 21:50:00 +0900

    Add input hidden

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
 <form name="contact" method="POST" netlify>
+  <input type="hidden" name="form-name" value="contact">
   <p>
     <label>Name <input type="text" name="name" /></label>
   </p>
@@ -9,22 +10,6 @@
   </p>
   <p>
     <button type="submit">Send</button>
-  </p>
-</form>
-
-<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" action="#">
-  <input type="hidden" name="form-name" value="contact">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button>Send</button>
   </p>
 </form>
 </div>
