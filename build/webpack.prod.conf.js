221813cd011e67b5dc984a5e803d78c3e73d7401
becolomochi <becolomochi@gmail.com>
2019-01-21 22:59:15 +0900

    Add Prerender url

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -126,7 +126,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       // Absolute path to compiled SPA
       path.resolve(__dirname, 'dist'),
       // List of routes to prerender
-      ['/']
+      ['/', '/success']
     )
   ]
 })
